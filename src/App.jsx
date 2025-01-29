ed9455384e46105861bbd1f5b6e05dd9f61309a6
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-01-29 20:07:31 +0200

    Created the vans component and added a route to the component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import About from "./pages/Vans"
+import Vans from './pages/Vans';
 
 function App() {
 
@@ -17,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
