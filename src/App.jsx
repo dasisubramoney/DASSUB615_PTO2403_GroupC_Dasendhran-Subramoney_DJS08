879973f32d0b097a6078c094935b0558a3551427
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-01-30 20:18:35 +0200

    Editied the components

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import About from "./pages/Vans"
 import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
+
+import "./Server"
 
 function App() {
 
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/Vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
