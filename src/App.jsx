c3825ebd652e426ec5bd492bfdaf484ae9d4975b
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-01-30 20:35:06 +0200

    Added the functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
+import Layout from "./Components/Layout"
 
 import "./Server"
 
@@ -13,10 +14,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/Vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element = {<Layout/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
