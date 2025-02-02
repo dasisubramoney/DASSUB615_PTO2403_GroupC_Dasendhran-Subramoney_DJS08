05cb1a5acbfa61ac24fd718454babdd3b51d76ad
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-02-02 16:14:04 +0200

    Added the serach filter

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -16,6 +16,10 @@ export default function Vans() {
             .then(data => setVans(data.vans))
     }, [])
 
+    const displayedVans = typeFilter
+    ? vans.filter(van => van.type === typeFilter)
+    : vans
+
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
@@ -32,6 +36,25 @@ export default function Vans() {
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
+            <div className="van-list-filter-buttons">
+                <Link 
+                    to="?type=simple"
+                    className="van-type simple"
+                >Simple</Link>
+                <Link 
+                    to="?type=luxury"
+                    className="van-type luxury"
+                >Luxury</Link>
+                <Link 
+                    to="?type=rugged"
+                    className="van-type rugged"
+                >Rugged</Link>
+                <Link 
+                    to="."
+                    className="van-type clear-filters"
+                >Clear filter</Link>
+            
+            </div>
             <div className="van-list">
                 {vanElements}
             </div>
